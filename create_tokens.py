a98d5fc711ce27b7f62fb323f47015edcfda5d44
laburnumT <laburnumtec@gmail.com>
2025-07-06 19:02:11 +0200

    Add automatic download option to create_tokens.py

--- create_tokens.py
+++ create_tokens.py
@@ -1,32 +1,68 @@
 import argparse
 import jinja2
-import sys
 import json
+import lxml.html
+import os
+import re
 import requests
+import sys
 
 
-def get_base_3():
+def get_base_3(role_info):
+    return [x["id"] for x in role_info if "Experimental" not in x["version"]]
+
+
+def get_asset(role, role_info):
+    try:
+        if os.path.isfile(f"assets/descriptions/{role}.txt"):
+            return
+    except:
+        pass
+
+    r_info = next((x for x in role_info if x["id"] == role))
     res = requests.get(
-        "https://script.bloodontheclocktower.com/data/roles.json"
+        f"https://script.bloodontheclocktower.com/{r_info['icon']}"
     )
-    return [x["id"] for x in res.json() if "Experimental" not in x["version"]]
+    with open(f"assets/icons/{role}.webp", "wb") as img:
+        img.write(res.content)
+    res = requests.get(
+        f"https://wiki.bloodontheclocktower.com/{r_info['name']}"
+    )
+    tree = lxml.html.fromstring(res.content)
+    des = re.sub(
+        r'^"(.*)"\n',
+        r"\1",
+        tree.xpath(
+            "/html/body/div[1]/div/section/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div/div[2]/div[1]/div[1]/p[1]/text()"
+        )[0],
+    )
+    with open(f"assets/descriptions/{role}.txt", "w") as description:
+        description.write(des)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--base-3", action="store_true")
+    parser.add_argument("--download", action="store_true")
     parser.add_argument("filename")
     args = parser.parse_args()
 
     with open(args.filename) as description:
         roles = json.load(description)[1::]
 
-    base3_roles = get_base_3()
+    role_info = requests.get(
+        "https://script.bloodontheclocktower.com/data/roles.json"
+    ).json()
+
+    base3_roles = get_base_3(role_info)
 
     l = []
     for role in roles:
         if not args.base_3 and role in base3_roles:
             continue
+
+        if args.download:
+            get_asset(role, role_info)
 
         try:
             with open(f"assets/descriptions/{role}.txt") as description:
